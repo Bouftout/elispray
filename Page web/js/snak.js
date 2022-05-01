0ff0fd5a5768a4dbe92b85ca28721115768791f3
ToniPortal <tonitoctoc13@gmail.com>
2022-05-01 16:45:39 +0200

    commit snake score

--- Page web/js/snak.js
+++ Page web/js/snak.js
@@ -71,17 +71,6 @@ function preload() {
 var retientgrow = [];
 
 function create() {
-    this.data.set('lives', 3);
-    this.data.set('level', 5);
-    this.data.set('score', 2000);
-
-    var text = this.add.text(100, 100, '', { font: '64px Courier', fill: '#00ff00' });
-
-    text.setText([
-        'Level: ' + this.data.get('level'),
-        'Lives: ' + this.data.get('lives'),
-        'Score: ' + this.data.get('score')
-    ]);
 
     var Food = new Phaser.Class({
         Extends: Phaser.GameObjects.Image,
